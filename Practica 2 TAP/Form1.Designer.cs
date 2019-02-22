b046d661d6633f15515251cd6f24767fbbb01dfc
Hickory <jared.ortega16@tectijuana.edu.mx>
2019-02-22 09:54:46 -0800

    logica basica agregada

--- Practica 2 TAP/Form1.Designer.cs
+++ Practica 2 TAP/Form1.Designer.cs
@@ -28,239 +28,117 @@ namespace Practica_2_TAP
         /// </summary>
         private void InitializeComponent()
         {
-            this.Tablazo = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
             this.btn_reinicio = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.Tablazo.SuspendLayout();
+            this.figurasGato = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
-            // 
-            // Tablazo
-            // 
-            this.Tablazo.Controls.Add(this.button9);
-            this.Tablazo.Controls.Add(this.button8);
-            this.Tablazo.Controls.Add(this.button7);
-            this.Tablazo.Controls.Add(this.button6);
-            this.Tablazo.Controls.Add(this.button5);
-            this.Tablazo.Controls.Add(this.button4);
-            this.Tablazo.Controls.Add(this.button3);
-            this.Tablazo.Controls.Add(this.button2);
-            this.Tablazo.Controls.Add(this.button1);
-            this.Tablazo.Location = new System.Drawing.Point(12, 107);
-            this.Tablazo.Name = "Tablazo";
-            this.Tablazo.Size = new System.Drawing.Size(555, 561);
-            this.Tablazo.TabIndex = 0;
-            this.Tablazo.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(6, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 174);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(188, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(176, 174);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(370, 21);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(176, 174);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(6, 201);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(176, 174);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(188, 201);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(176, 174);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(370, 201);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(176, 174);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(6, 381);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(176, 174);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(188, 381);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(176, 174);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(370, 381);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(176, 174);
-            this.button9.TabIndex = 8;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
             // 
             // btn_reinicio
             // 
-            this.btn_reinicio.Location = new System.Drawing.Point(222, 12);
+            this.btn_reinicio.Location = new System.Drawing.Point(166, 10);
+            this.btn_reinicio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_reinicio.Name = "btn_reinicio";
-            this.btn_reinicio.Size = new System.Drawing.Size(101, 88);
+            this.btn_reinicio.Size = new System.Drawing.Size(76, 72);
             this.btn_reinicio.TabIndex = 1;
             this.btn_reinicio.Text = "Boton de Reinicio";
             this.btn_reinicio.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(619, 60);
+            this.label1.Location = new System.Drawing.Point(464, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 40);
+            this.label1.Size = new System.Drawing.Size(110, 32);
             this.label1.TabIndex = 2;
             this.label1.Text = "Jugador 1";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(771, 60);
+            this.label2.Location = new System.Drawing.Point(578, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 40);
+            this.label2.Size = new System.Drawing.Size(110, 32);
             this.label2.TabIndex = 3;
             this.label2.Text = "Jugador 2";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(619, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 150);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Inserte Imagen del jugador 1 aqui";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.Location = new System.Drawing.Point(771, 107);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 150);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Inserte Imagen del jugador 2 aqui";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(619, 501);
+            this.label5.Location = new System.Drawing.Point(464, 407);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(146, 61);
+            this.label5.Size = new System.Drawing.Size(110, 50);
             this.label5.TabIndex = 6;
             this.label5.Text = "Ganadas del Jugador 1";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(771, 501);
+            this.label6.Location = new System.Drawing.Point(578, 407);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(146, 61);
+            this.label6.Size = new System.Drawing.Size(110, 50);
             this.label6.TabIndex = 7;
             this.label6.Text = "Ganadas del Jugador 2";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label7
             // 
-            this.label7.Location = new System.Drawing.Point(695, 440);
+            this.label7.Location = new System.Drawing.Point(521, 358);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(146, 61);
+            this.label7.Size = new System.Drawing.Size(110, 50);
             this.label7.TabIndex = 8;
             this.label7.Text = "Marcador ";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // figurasGato
+            // 
+            this.figurasGato.ColumnCount = 3;
+            this.figurasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.figurasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.figurasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.figurasGato.Location = new System.Drawing.Point(12, 105);
+            this.figurasGato.Name = "figurasGato";
+            this.figurasGato.RowCount = 3;
+            this.figurasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.74468F));
+            this.figurasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 54.25532F));
+            this.figurasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+            this.figurasGato.Size = new System.Drawing.Size(420, 420);
+            this.figurasGato.TabIndex = 9;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 680);
+            this.ClientSize = new System.Drawing.Size(762, 552);
+            this.Controls.Add(this.figurasGato);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_reinicio);
-            this.Controls.Add(this.Tablazo);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Tablazo.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox Tablazo;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_reinicio;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TableLayoutPanel figurasGato;
     }
 }
 
--- Practica 2 TAP/Form1.cs
+++ Practica 2 TAP/Form1.cs
@@ -16,6 +16,15 @@ namespace Practica_2_TAP
     public partial class Form1 : Form
     {
         bool sim = true;
+        int turno = 1;
+        int[,] Gato;
+        int puntosJ1 = 0;
+        int puntosJ2 = 0;
+        string NomJ1;
+        string NomJ2;
+        bool hayGanador;
+        int turnoContador = 0;
+        int empate = 0;
 
         public Form1()
         {
@@ -25,12 +34,188 @@ namespace Practica_2_TAP
 
         public void iniciojuego()
         {
-            int[,] matrizactual = new int [3,3];
+            Gato = new int [3,3];
             int turno = 1;
+            hayGanador = false;
+
+            figurasGato.Controls.Clear();
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    var FiguraJuego = new PictureBox();
+                    FiguraJuego.Image = Properties.Resources.Cuadrado;
+                    FiguraJuego.Name = string.Format("{0}", i + " " + j);
+                    FiguraJuego.Dock = DockStyle.Fill;
+                    FiguraJuego.Cursor = Cursors.Hand;
+                    FiguraJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    FiguraJuego.Click += Jugar;
+                    figurasGato.Controls.Add(FiguraJuego, j, i);
+                    Gato[i, j] = 0;
+                }
+            }
 
         }
 
-        publix
+        private void Jugar(object sender, EventArgs e)
+        {
+            var figuraSeleccionada = (PictureBox)sender;
+            if (turno == 1)
+            {
+                figuraSeleccionada.Image = Properties.Resources.X;
+            }
+            else
+            {
+                figuraSeleccionada.Image = Properties.Resources.O;
+            }
+            string[] Posicion = figuraSeleccionada.Name.Split(" ".ToCharArray());
+            int Fila = Convert.ToInt32(Posicion[0]);
+            int Columna = Convert.ToInt32(Posicion[1]);
+            Gato[Fila, Columna] = turno;
+            VerificarJuego(Fila, Columna);
+            turno = (turno == 1) ? 2 : 1;
+        }
+
+        public void VerificarJuego(int Fila, int Columna)
+        {//Esto sirve para contar las figuras en las filas y las columnas
+            int GFilas = 0;
+            int GColumnas = 0;
+            int DiagonalPrin = 0;
+            int DiagonalInv = 0;
+            int tamGato = 3;
+
+            for (var i = 0; i < tamGato; i++)
+            {
+                for (var j = 0; j < tamGato; j++)
+                {
+                    if (i == Fila)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GFilas++;
+
+                            turnoContador++;
+
+                        }
+                    }
+                    if (j == Columna)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GColumnas++;
+                            turnoContador++;
+                        }
+                    }
+                    if (i == j)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalPrin++;
+                            turnoContador++;
+                        }
+                    }
+                    if ((i + j) == (tamGato - 1))
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalInv++;
+                            turnoContador++;
+                        }
+                    }
+                }
+            }
+
+            if ((GFilas == tamGato) || (GColumnas == tamGato) || (DiagonalInv == tamGato) || (DiagonalPrin == tamGato))
+            {
+                hayGanador = true;
+            }
+
+            if (hayGanador)
+            {
+                string Ganador;
+                MessageBox.Show("Hay un Ganador!");
+                if (turno == 1)
+                {
+                    Ganador = NomJ1;
+                    TextoGanador.Text = Ganador;
+                    puntosJ1++;
+                    ScoreJ1.Text = puntosJ1.ToString();
+                }
+                else if (turno == 2)
+                {
+                    Ganador = NomJ2;
+                    TextoGanador.Text = Ganador;
+                    puntosJ2++;
+                    ScoreJ2.Text = puntosJ2.ToString();
+                }
+                else
+                {
+                    if (turnoContador == 9)
+                    {
+                        MessageBox.Show("Es un empate!");
+                        empate++;
+                        Empates.Text = "Es un empate!";
+                    }
+                }
+            }
+        }
+        private void Reiniciar_Click(object sender, EventArgs e)
+        {
+            InicioJuego();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextoGanador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EntradaNombreJ1_TextChanged(object sender, EventArgs e)
+        {
+            NomJ1 = EntradaNombreJ1.Text;
+        }
+
+        private void EntradaNombreJ2_TextChanged(object sender, EventArgs e)
+        {
+            NomJ2 = EntradaNombreJ2.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InicioJuego();
+        }
+
+        private void Empates_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
         //Al chile no entendi lo que querias hacer pero lo guardare y lo usare de referencia
         /*private void boton1_clic(object sender, EventArgs pizo)
         {
@@ -42,7 +227,7 @@ namespace Practica_2_TAP
             else
             {
                 sim = true;
-                button1.Image = Image.FromFile("pues ya saves no");
+                button1.Image = Image.FromFile("pues ya sabes no");
                 button1.Enabled = false;
             }
         }
